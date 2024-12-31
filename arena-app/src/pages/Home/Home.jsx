4a15559ed8a012a1b6e4ba2d9c903c276cae1bb9
Glory <gloryope247@gmail.com>
2024-12-31 01:40:49 +0100

    conti

--- arena-app/src/pages/Home/Home.jsx
+++ arena-app/src/pages/Home/Home.jsx
@@ -1,12 +1,25 @@
 import React from 'react'
 import './Home.css'
 import Navbar from '../../components/Navbar/Navbar'
+import hero_banner from '../../assets/hero_banner.jpg'
+import title from '../../assets/title.png'
+
 
 const Home = () => {
   return (
     <div className='home'>
       <Navbar />
-      
+      <div className="hero">
+        <img src={hero_banner} alt="" className="hero_banner" width={1000} height={1000} />
+        <div className="hero-caption"></div>
+        <img src={title} alt="" className="caption-img" width={100}height={100}/>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, 
+          vitae esse! Error impedit vero molestiae itaque nobis 
+          </p>
+          <div className="hero-btns">
+            <button><img src="" alt="" /></button>
+          </div>
+      </div>
     </div>
   )
 }
